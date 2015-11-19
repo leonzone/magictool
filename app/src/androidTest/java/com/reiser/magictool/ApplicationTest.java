7db53588bfcd91f15346e0675366a7fb41660136
reiserx <reiserx@163.com>
2015-11-19 17:32:09 +0800

    First commit

---
+++ app/src/androidTest/java/com/reiser/magictool/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.reiser.magictool;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
